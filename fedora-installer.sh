27199c04e8c56d4ef57db729b1fd5365d6a4a941
winux <78425126+Pedro1234-code@users.noreply.github.com>
2021-11-08 23:34:25 +0000

    Create fedora-installer.sh

---
+++ fedora-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Setup
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-beta.repo
+## Install
+sudo dnf install microsoft-edge-stable
+
+echo Installed. Check your apps menu.
